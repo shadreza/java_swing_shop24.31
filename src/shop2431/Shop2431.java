a7b953f68d878667d0dd13de4916a81f095d2dd9
ASUS <ASUS@192.168.0.100>
2020-10-10 01:30:30 +0600

    This is a shop management swing project

---
+++ src/shop2431/Shop2431.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shop2431;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Shop2431 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        welcome shop = new welcome();
+        shop.show();
+    }
+    
+}
